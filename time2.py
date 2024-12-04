657a5081f8f2afb8fa8d26859434a8e35b2960fa
Brandon Ortiz <bortiz6@my.nl.edu>
2024-12-04 17:18:23 -0600

    Add files via upload

---
+++ time2.py
@@ -0,0 +1,7 @@
+str_time = input("What time is it now?")
+str_wait_time = input("What is the number of nours to wait?")
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_go_off = (time + wait_time) % 24
+print("time when alarm go off at", time_when_alarm_go_off)
